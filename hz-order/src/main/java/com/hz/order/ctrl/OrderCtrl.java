42bf1dd0e58c6d0231257693a93716d2d2199d4c
rhb <bashu.rhb@raycloud.com>
2025-08-27 09:42:23 +0800

    seata的客户端配置

--- hz-order/src/main/java/com/hz/order/ctrl/OrderCtrl.java
+++ hz-order/src/main/java/com/hz/order/ctrl/OrderCtrl.java
@@ -2,6 +2,9 @@ package com.hz.order.ctrl;
 
 import com.hz.base.api.OrderApi;
 import com.hz.base.entity.OrderInfo;
+import io.seata.core.context.RootContext;
+import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
  * @date 2025-08-18 星期一 18:48:08
  * @description
  */
+@Slf4j
 @RestController
 public class OrderCtrl implements OrderApi {
     @Override
@@ -18,8 +22,13 @@ public class OrderCtrl implements OrderApi {
         return null;
     }
 
+    @GlobalTransactional(timeoutMills = 300000, name = "spring-cloud-demo-tx")
     @Override
     public Boolean placeOrder() {
+        String xid = RootContext.getXID();
+        boolean gTransaction = RootContext.inGlobalTransaction();
+        log.info("xid:{}, gTransaction:{}",xid, gTransaction);
+
         return null;
     }
 }
